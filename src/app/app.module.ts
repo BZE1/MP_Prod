a40b73dd988ce0aa28510c62634c0064502e70e1
TSN Student <student@ulem.org>
2018-05-21 11:22:36 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,41 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { PageNewComponent } from './components/page/page-new/page-new.component';
+import { PageEditComponent } from './components/page/page-edit/page-edit.component';
+import { PageListComponent } from './components/page/page-list/page-list.component';
+import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
+import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
+import { WebsiteListComponent } from './components/website/website-list/website-list.component';
+import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
+import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
+import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
+import { WidgetImageComponent } from './components/widget/widget-image/widget-image.component';
+import { WidgetYoutubeComponent } from './components/widget/widget-youtube/widget-youtube.component';
+import { WidgetHeaderComponent } from './widget-header/widget-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    PageNewComponent,
+    PageEditComponent,
+    PageListComponent,
+    WebsiteNewComponent,
+    WebsiteEditComponent,
+    WebsiteListComponent,
+    WidgetChooserComponent,
+    WidgetEditComponent,
+    WidgetListComponent,
+    WidgetImageComponent,
+    WidgetYoutubeComponent,
+    WidgetHeaderComponent
   ],
   imports: [
     BrowserModule
